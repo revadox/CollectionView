01d4478e2828e5a2a9e6406636b1b77b799b3797
vd <vdbazinga79@gmail.com>
2018-01-05 19:43:04 +0530

    add double tap to zoom and pinch to zoom

--- CollectionView/ShowImage.swift
+++ CollectionView/ShowImage.swift
@@ -8,27 +8,59 @@
 
 import UIKit
 
-class ShowImage: UIViewController {
+class ShowImage: UIViewController,UIScrollViewDelegate {
 
  
     
- 
+    @IBOutlet var imageScrollView: UIScrollView!
+    
     @IBOutlet var showImage: UIImageView!
     
+    @IBAction func zoomImage(_ sender: Any) {
+        
+        if (imageScrollView.zoomScale < 1.5) {
+            
+            //2
+            imageScrollView.setZoomScale(imageScrollView.maximumZoomScale, animated: true)
+            
+        } else {
+            
+            //3
+            imageScrollView.setZoomScale(imageScrollView.minimumZoomScale, animated: true)
+            
+        }
+        
+        
+  
+        
+    }
     
     var image = UIImage()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    showImage.image = image
+        showImage.image = image
         navigationItem.title = "image"
+        
+        self.imageScrollView.isUserInteractionEnabled = true
+        self.imageScrollView.minimumZoomScale = 1.0
+        self.imageScrollView.maximumZoomScale = 6.0
        
     }
+    
+    
+   
+        
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.showImage
     }
     
 
